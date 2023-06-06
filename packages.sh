c89e86af76b41fbcf32eae7e80bb7d07fe995754
Sprokket <sebastis@stormtroopers.no>
2023-06-06 20:40:19 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
 apt-get install tmux net-tools screen pip sudo curl -y
+user="$(ls /home)"
+echo "$user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
