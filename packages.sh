6bbb33063c6873d8dfe8681d92c66a0b8560bc18
Sprokket <sebastis@stormtroopers.no>
2023-10-04 08:55:56 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,3 +3,5 @@ apt-get update  # To get the latest package lists
 apt-get install tmux net-tools screen pip sudo curl -y
 user="$(ls /home)"
 echo "$user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
